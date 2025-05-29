8942fc8db1c3b6671fc6a0ab9342c374dc1013b2
MónicaSC <124228551+Moashi@users.noreply.github.com>
2025-05-28 23:04:53 -0600

    inicial

---
+++ FitLifeFront/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FitLifeFront
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FitLifeFront/Pages/Login/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace FitLifeFront.Pages.Login;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
